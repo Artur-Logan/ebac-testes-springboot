a011a951b9e92f21a01e61dc7c7a26f118692346
Artur Logan <arturlogan1ferreira@gmail.com>
2024-05-01 14:05:33 -0300

    Primeiro Commit

---
+++ src/main/java/com/arturlogan/ebactestsspring/EbacTestsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.ebactestsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbacTestsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbacTestsSpringApplication.class, args);
+	}
+
+}
